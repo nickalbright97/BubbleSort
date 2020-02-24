11a4f05ddc0e06effc9c0edbf16560c70d5c732b
Nick <albrigne@login01.cluster.cs.jmu.edu>
2020-02-24 15:22:33 -0500

    Added image import and size grabbing info

---
+++ Makefile
@@ -0,0 +1,2 @@
+import: import.c
+	gcc -ltiff -std=c99 -o import import.c -I.
--- import.c
+++ import.c
@@ -1,8 +1,41 @@
 #include "tiffio.h"
-main()
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
 {
-    TIFF* tif = TIFFOpen("img100_1.tif", "r");
-    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);           // uint32 width;
-    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);        // uint32 height;
+/* This .tiff file is essentially a stack of 2D images - this code tells us how many images
+ * are in the stack.
+ */
+    TIFF *tif = TIFFOpen("img100_1.tif", "r");
+    if (tif) {
+	int dircount = 0;
+	do {
+	    dircount++;
+	} while (TIFFReadDirectory(tif));
+	printf("%d directories in %s\n", dircount, "img100_1");
+    }
+
+    if (tif) {
+	uint32 w, h;
+	size_t npixels;
+	uint32* raster;
+
+	TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
+	TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
+        printf("TIFF width: %d\n", w);
+        printf("TIFF height: %d\n", h);
+	npixels = w * h;
+	raster = (uint32*) _TIFFmalloc(npixels * sizeof (uint32));
+	if (raster != NULL) {
+	    if (TIFFReadRGBAImage(tif, w, h, raster, 0)) {
+                // manipulate raster data here
+	    }
+	    _TIFFfree(raster);
+	}
+    }
+
+
     TIFFClose(tif);
+    exit(0);
 }
